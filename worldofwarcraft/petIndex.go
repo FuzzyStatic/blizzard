5fd03b9d3f87ddaed9aa626daf70a6f36bc15d68
Allen Flickinger <allen.flickinger@gmail.com>
2018-01-15 12:35:25 -0500

    Move to pet index

--- worldofwarcraft/petIndex.go
+++ worldofwarcraft/petIndex.go
@@ -2,15 +2,15 @@
  * @Author: Allen Flickinger (allen.flickinger@gmail.com)
  * @Date: 2018-01-14 21:44:17
  * @Last Modified by: FuzzyStatic
- * @Last Modified time: 2018-01-14 21:51:41
+ * @Last Modified time: 2018-01-15 12:12:13
  */
 
 package worldofwarcraft
 
 import "encoding/json"
 
-// Pet structure
-type Pet struct {
+// PetIndex structure
+type PetIndex struct {
 	Pets []struct {
 		CanBattle  bool   `json:"canBattle"`
 		CreatureID int    `json:"creatureId"`
@@ -33,7 +33,7 @@ type Pet struct {
 	} `json:"pets"`
 }
 
-// JSON2Struct creates Pet structure from JSON byte array
-func (p *Pet) JSON2Struct(b *[]byte) error {
+// JSON2Struct creates PetIndex structure from JSON byte array
+func (p *PetIndex) JSON2Struct(b *[]byte) error {
 	return json.Unmarshal(*b, p)
 }
