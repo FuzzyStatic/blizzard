775da2e91776ce718609da6195bbc0af89e714e6
FuzzyStatic <allen.flickinger@gmail.com>
2019-01-06 17:04:49 -0500

    Remove unused import

--- init_test.go
+++ init_test.go
@@ -4,20 +4,9 @@ import (
 	"fmt"
 	"log"
 	"os"
-
-	"github.com/spf13/viper"
 )
 
 func init() {
-	viper.SetConfigName("credentials") // replace with TOML file similar to sample.toml
-	viper.SetConfigType("toml")
-	viper.AddConfigPath("./configs")
-
-	err := viper.ReadInConfig()
-	if err != nil {
-		fmt.Println(err)
-	}
-
 	clientID := os.Getenv("CLIENT_ID")
 	if clientID == "" {
 		log.Fatal("Set the environment variable CLIENT_ID before retrying.")
@@ -30,7 +19,7 @@ func init() {
 
 	c = NewClient(clientID, clientSecret, US, enUS)
 
-	err = c.AccessTokenReq()
+	err := c.AccessTokenReq()
 	if err != nil {
 		fmt.Println(err)
 		return
