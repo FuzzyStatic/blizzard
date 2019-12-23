a2147f0e9204e452956db5602df55aa74b9166df
FuzzyStatic <allen.flickinger@gmail.com>
2019-12-23 17:05:44 -0500

    ✨ Add new structures
    git push

---
+++ wowp/characterAchievements.go
@@ -0,0 +1,48 @@
+package wowp
+
+// CharacterAchievementsSummary structure
+type CharacterAchievementsSummary struct {
+	Links struct {
+		Self struct {
+			Href string `json:"href"`
+		} `json:"self"`
+	} `json:"_links"`
+	TotalQuantity int `json:"total_quantity"`
+	TotalPoints   int `json:"total_points"`
+	Achievements  []struct {
+		ID          int `json:"id,omitempty"`
+		Achievement struct {
+			Key struct {
+				Href string `json:"href"`
+			} `json:"key"`
+			Name string `json:"name"`
+			ID   int    `json:"id"`
+		} `json:"achievement"`
+		Criteria struct {
+			ID            int  `json:"id"`
+			Amount        int  `json:"amount"`
+			IsCompleted   bool `json:"is_completed"`
+			ChildCriteria []struct {
+				ID          int  `json:"id"`
+				Amount      int  `json:"amount"`
+				IsCompleted bool `json:"is_completed"`
+			} `json:"child_criteria"`
+		} `json:"criteria,omitempty"`
+		CompleteTimestamp int64 `json:"completed_timestamp"`
+	} `json:"achievements"`
+	Character struct {
+		Key struct {
+			Href string `json:"href"`
+		} `json:"key"`
+		Name  string `json:"name"`
+		ID    int    `json:"id"`
+		Realm struct {
+			Key struct {
+				Href string `json:"href"`
+			} `json:"key"`
+			Name string `json:"name"`
+			ID   int    `json:"id"`
+			Slug string `json:"slug"`
+		} `json:"realm"`
+	} `json:"character"`
+}
