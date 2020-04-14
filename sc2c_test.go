03be82a6fb825ffa9a08578c8dbef41f807b86a9
FuzzyStatic <allen.flickinger@gmail.com>
2020-04-13 23:19:26 -0400

    ✅ add backsc2 unit testing

--- sc2c_test.go
+++ sc2c_test.go
@@ -1,169 +1,174 @@
 package blizzard
 
-// func TestSC2StaticProfile(t *testing.T) {
-// 	dat, _, err := c.SC2StaticProfile(US)
-// 	if err != nil {
-// 		fmt.Println(err)
-// 		t.Fail()
-// 	}
+import (
+	"fmt"
+	"testing"
+)
 
-// 	if printOutput != "" {
-// 		fmt.Printf("%+v\n", dat)
-// 	}
-// }
+func TestSC2StaticProfile(t *testing.T) {
+	dat, _, err := c.SC2StaticProfile(US)
+	if err != nil {
+		fmt.Println(err)
+		t.Fail()
+	}
 
-// func TestSC2MetadataProfile(t *testing.T) {
-// 	dat, _, err := c.SC2MetadataProfile(US, 1, 305084)
-// 	if err != nil {
-// 		fmt.Println(err)
-// 		t.Fail()
-// 	}
+	if printOutput != "" {
+		fmt.Printf("%+v\n", dat)
+	}
+}
 
-// 	if printOutput != "" {
-// 		fmt.Printf("%+v\n", dat)
-// 	}
-// }
+func TestSC2MetadataProfile(t *testing.T) {
+	dat, _, err := c.SC2MetadataProfile(US, 1, 305084)
+	if err != nil {
+		fmt.Println(err)
+		t.Fail()
+	}
 
-// func TestSC2Profile(t *testing.T) {
-// 	dat, _, err := c.SC2Profile(US, 1, 305084)
-// 	if err != nil {
-// 		fmt.Println(err)
-// 		t.Fail()
-// 	}
+	if printOutput != "" {
+		fmt.Printf("%+v\n", dat)
+	}
+}
 
-// 	if printOutput != "" {
-// 		fmt.Printf("%+v\n", dat)
-// 	}
-// }
+func TestSC2Profile(t *testing.T) {
+	dat, _, err := c.SC2Profile(US, 1, 305084)
+	if err != nil {
+		fmt.Println(err)
+		t.Fail()
+	}
 
-// func TestSC2ProfileLadderSummary(t *testing.T) {
-// 	dat, _, err := c.SC2ProfileLadderSummary(US, 1, 305084)
-// 	if err != nil {
-// 		fmt.Println(err)
-// 		t.Fail()
-// 	}
+	if printOutput != "" {
+		fmt.Printf("%+v\n", dat)
+	}
+}
 
-// 	if printOutput != "" {
-// 		fmt.Printf("%+v\n", dat)
-// 	}
-// }
+func TestSC2ProfileLadderSummary(t *testing.T) {
+	dat, _, err := c.SC2ProfileLadderSummary(US, 1, 305084)
+	if err != nil {
+		fmt.Println(err)
+		t.Fail()
+	}
 
-// func TestSC2ProfileLadder(t *testing.T) {
-// 	dat, _, err := c.SC2ProfileLadder(US, 1, 2376042, 194163)
-// 	if err != nil {
-// 		fmt.Println(err)
-// 		t.Fail()
-// 	}
+	if printOutput != "" {
+		fmt.Printf("%+v\n", dat)
+	}
+}
 
-// 	if printOutput != "" {
-// 		fmt.Printf("%+v\n", dat)
-// 	}
-// }
+func TestSC2ProfileLadder(t *testing.T) {
+	dat, _, err := c.SC2ProfileLadder(US, 1, 2376042, 194163)
+	if err != nil {
+		fmt.Println(err)
+		t.Fail()
+	}
 
-// func TestSC2LadderGrandmaster(t *testing.T) {
-// 	dat, _, err := c.SC2LadderGrandmaster(EU)
-// 	if err != nil {
-// 		fmt.Println(err)
-// 		t.Fail()
-// 	}
+	if printOutput != "" {
+		fmt.Printf("%+v\n", dat)
+	}
+}
 
-// 	if printOutput != "" {
-// 		fmt.Printf("%+v\n", dat)
-// 	}
-// }
+func TestSC2LadderGrandmaster(t *testing.T) {
+	dat, _, err := c.SC2LadderGrandmaster(EU)
+	if err != nil {
+		fmt.Println(err)
+		t.Fail()
+	}
 
-// func TestSC2LadderSeason(t *testing.T) {
-// 	dat, _, err := c.SC2LadderSeason(EU)
-// 	if err != nil {
-// 		fmt.Println(err)
-// 		t.Fail()
-// 	}
+	if printOutput != "" {
+		fmt.Printf("%+v\n", dat)
+	}
+}
 
-// 	if printOutput != "" {
-// 		fmt.Printf("%+v\n", dat)
-// 	}
-// }
+func TestSC2LadderSeason(t *testing.T) {
+	dat, _, err := c.SC2LadderSeason(EU)
+	if err != nil {
+		fmt.Println(err)
+		t.Fail()
+	}
 
-// func TestSC2Player(t *testing.T) {
-// 	dat, _, err := c.SC2Player(1312411)
-// 	if err != nil {
-// 		fmt.Println(err)
-// 		t.Fail()
-// 	}
+	if printOutput != "" {
+		fmt.Printf("%+v\n", dat)
+	}
+}
 
-// 	if printOutput != "" {
-// 		fmt.Printf("%+v\n", dat)
-// 	}
-// }
+func TestSC2Player(t *testing.T) {
+	dat, _, err := c.SC2Player(1312411)
+	if err != nil {
+		fmt.Println(err)
+		t.Fail()
+	}
 
-// func TestSC2LegacyProfile(t *testing.T) {
-// 	dat, _, err := c.SC2LegacyProfile(US, 1, 1655091)
-// 	if err != nil {
-// 		fmt.Println(err)
-// 		t.Fail()
-// 	}
+	if printOutput != "" {
+		fmt.Printf("%+v\n", dat)
+	}
+}
 
-// 	if printOutput != "" {
-// 		fmt.Printf("%+v\n", dat)
-// 	}
-// }
+func TestSC2LegacyProfile(t *testing.T) {
+	dat, _, err := c.SC2LegacyProfile(US, 1, 1655091)
+	if err != nil {
+		fmt.Println(err)
+		t.Fail()
+	}
 
-// func TestSC2LegacyProfileLadders(t *testing.T) {
-// 	dat, _, err := c.SC2LegacyProfileLadders(US, 1, 1655091)
-// 	if err != nil {
-// 		fmt.Println(err)
-// 		t.Fail()
-// 	}
+	if printOutput != "" {
+		fmt.Printf("%+v\n", dat)
+	}
+}
 
-// 	if printOutput != "" {
-// 		fmt.Printf("%+v\n", dat)
-// 	}
-// }
+func TestSC2LegacyProfileLadders(t *testing.T) {
+	dat, _, err := c.SC2LegacyProfileLadders(US, 1, 1655091)
+	if err != nil {
+		fmt.Println(err)
+		t.Fail()
+	}
 
-// func TestSC2LegacyProfileMatches(t *testing.T) {
-// 	dat, _, err := c.SC2LegacyProfileMatches(US, 1, 1655091)
-// 	if err != nil {
-// 		fmt.Println(err)
-// 		t.Fail()
-// 	}
+	if printOutput != "" {
+		fmt.Printf("%+v\n", dat)
+	}
+}
 
-// 	if printOutput != "" {
-// 		fmt.Printf("%+v\n", dat)
-// 	}
-// }
+func TestSC2LegacyProfileMatches(t *testing.T) {
+	dat, _, err := c.SC2LegacyProfileMatches(US, 1, 1655091)
+	if err != nil {
+		fmt.Println(err)
+		t.Fail()
+	}
 
-// func TestSC2LegacyLadder(t *testing.T) {
-// 	dat, _, err := c.SC2LegacyLadder(US, 194163)
-// 	if err != nil {
-// 		fmt.Println(err)
-// 		t.Fail()
-// 	}
+	if printOutput != "" {
+		fmt.Printf("%+v\n", dat)
+	}
+}
 
-// 	if printOutput != "" {
-// 		fmt.Printf("%+v\n", dat)
-// 	}
-// }
+func TestSC2LegacyLadder(t *testing.T) {
+	dat, _, err := c.SC2LegacyLadder(US, 194163)
+	if err != nil {
+		fmt.Println(err)
+		t.Fail()
+	}
 
-// func TestSC2LegacyAchievements(t *testing.T) {
-// 	dat, _, err := c.SC2LegacyAchievements(US)
-// 	if err != nil {
-// 		fmt.Println(err)
-// 		t.Fail()
-// 	}
+	if printOutput != "" {
+		fmt.Printf("%+v\n", dat)
+	}
+}
 
-// 	if printOutput != "" {
-// 		fmt.Printf("%+v\n", dat)
-// 	}
-// }
+func TestSC2LegacyAchievements(t *testing.T) {
+	dat, _, err := c.SC2LegacyAchievements(US)
+	if err != nil {
+		fmt.Println(err)
+		t.Fail()
+	}
 
-// func TestSC2LegacyRewards(t *testing.T) {
-// 	dat, _, err := c.SC2LegacyRewards(US)
-// 	if err != nil {
-// 		fmt.Println(err)
-// 		t.Fail()
-// 	}
+	if printOutput != "" {
+		fmt.Printf("%+v\n", dat)
+	}
+}
 
-// 	if printOutput != "" {
-// 		fmt.Printf("%+v\n", dat)
-// 	}
-// }
+func TestSC2LegacyRewards(t *testing.T) {
+	dat, _, err := c.SC2LegacyRewards(US)
+	if err != nil {
+		fmt.Println(err)
+		t.Fail()
+	}
+
+	if printOutput != "" {
+		fmt.Printf("%+v\n", dat)
+	}
+}
