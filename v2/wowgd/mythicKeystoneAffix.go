3e3b17b5767c5dc93d78e48f4a14258ab59c0517
Novikov Roman <service.for.roman@gmail.com>
2021-02-15 11:23:40 +0300

    add WoWMythicKeystoneAffixMedia

--- v2/wowgd/mythicKeystoneAffix.go
+++ v2/wowgd/mythicKeystoneAffix.go
@@ -33,3 +33,18 @@ type MythicKeystoneAffix struct {
 		ID int `json:"id"`
 	} `json:"media"`
 }
+
+// MythicKeystoneAffixMedia structure
+type MythicKeystoneAffixMedia struct {
+	Links struct {
+		Self struct {
+			Href string `json:"href"`
+		} `json:"self"`
+	} `json:"_links"`
+	Assets []struct {
+		Key        string `json:"key"`
+		Value      string `json:"value"`
+		FileDataID int    `json:"file_data_id"`
+	} `json:"assets"`
+	ID int `json:"id"`
+}
