1496ca97f8921e1b29a47f84b2ecad3126e6bbfc
FuzzyStatic <allen.flickinger@gmail.com>
2021-03-25 21:17:45 -0400

    🚑 add missing AH fields

--- v2/wowgd/auctionHouse.go
+++ v2/wowgd/auctionHouse.go
@@ -13,8 +13,19 @@ type AuctionHouse struct {
 	Auctions []struct {
 		ID   int `json:"id"`
 		Item struct {
-			ID int `json:"id"`
+			ID         int   `json:"id"`
+			Context    int   `json:"context"`
+			BonusLists []int `json:"bonus_lists"`
+			Modifiers  []struct {
+				Type  int `json:"type"`
+				Value int `json:"value"`
+			} `json:"modifiers"`
+			PetBreedID   int `json:"pet_breed_id"`
+			PetLevel     int `json:"pet_level"`
+			PetQualityID int `json:"pet_quality_id"`
+			PetSpeciesID int `json:"pet_species_id"`
 		} `json:"item"`
+		Buyout    int      `json:"buyout"`
 		Quantity  int      `json:"quantity"`
 		UnitPrice int      `json:"unit_price"`
 		TimeLeft  TimeLeft `json:"time_left"`
